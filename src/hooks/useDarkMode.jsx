614c10282f21078aedf2694b580e3d2a12b05f58
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-30 12:18:05 -0700

    Dark mode feature works, but it is not saving to localstorage

---
+++ src/hooks/useDarkMode.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage(false);
+
+  useEffect(() => {
+    darkMode
+      ? document.body.classList.add("dark-mode")
+      : document.body.classList.remove("dark-mode");
+  }, [darkMode]);
+
+  return [darkMode, setDarkMode];
+};
